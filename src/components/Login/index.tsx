046c3dcfe50552ad8e88195411699c727aec6212
Otávio Camargo <45431946+otcamargo@users.noreply.github.com>
2021-03-08 23:59:28 -0300

    Implement login component (#3)
    * Implement styles for password field
    
    * Setup api service using axios
    
    * Finish implementing login component api interactions

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import {FiLogIn} from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    // TODO: check if this is really necessary
+    const { username, password } = formData;
+
+    const res = await api.post('/auth/login', formData).then(res => res.data);
+    alert('Login efetuado!');
+  }
+
+
   return (
     <div id="page-login">
       <div className="content">
@@ -13,17 +37,17 @@ const Login = () => {
         </header>
 
         <div className="form-box">
-          <form /*onSubmit={handleSubmit}*/>
+          <form onSubmit={handleSubmit}>
             <h1>Faça login ou crie uma conta</h1>
 
             <fieldset>
               <div className="field">
                 <label htmlFor="username">Nome de usuário</label>
-                <input type="text" name="username" id="username" /*onChange={handleInputChange}*//>
+                <input type="text" name="username" id="username" onChange={handleInputChange}/>
               </div>
               <div className="field">
                 <label htmlFor="password">Senha</label>
-                <input type="text" name="password" id="password" /*onChange={handleInputChange}*//>
+                <input type="password" name="password" id="password" onChange={handleInputChange}/>
               </div>
 
               <button type="submit">
